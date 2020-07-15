ed2459e601fd6d0cf64efdf28275ea638f5c7873
best9076 <best9076@naver.com>
2020-07-15 16:26:00 +0900

    woojin - dao.AdminDAO 파일 이름 수정 및 포트폴리오 테이블 생성

--- FinalEeum/src/main/java/com/kh/eeum/dao/AdminDAO.java
+++ FinalEeum/src/main/java/com/kh/eeum/dao/AdminDAO.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class Admin {
+public class AdminDAO {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
---
+++ FinalEeum/src/main/webapp/resources/sql/portfolio.sql
@@ -0,0 +1,24 @@
+
+--create table expert portfolio
+DROP TABLE PORTFOLIO;
+
+CREATE TABLE PORTFOLIO(
+	PF_EXID VARCHAR2(40) NOT NULL,
+	PF_LOC VARCHAR2(300),
+	PF_GRADE NUMBER NOT NULL,--0-디딤돌, 1-마루, 2-우주, 3-용마루
+	PF_CATE NUMBER NOT NULL,--0-청소, 1-방역, 2-수리
+	PF_TIME VARCHAR2(100) NOT NULL,
+	PF_DESC CLOB,
+	PF_LI CLOB,
+	PF_OP CLOB,
+	PF_INTRO VARCHAR2(300) NOT NULL,
+	PF_One VARCHAR2(100)
+);
+
+ALTER TABLE PORTFOLIO
+ADD CONSTRAINT PF_EXID FOREIGN KEY(PF_EXID)
+REFERENCES EXPERT_TABLE(EXPERT_ID);
+
+insert into PORTFOLIO
+values('user5678', '서울시 동대문구 전농동', 0, 0, '09:00-18:00', '웅앵웅웅앵앵우ㅐ애ㅜ앨만', null, null, '피곤해 죽겠읍니다. ', '세상에서 가장 좋은 건? 집');
+
