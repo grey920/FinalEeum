5a780a0236e325394f48ca30d073e0d28edad7b3
hansol <hansol302@gmail.com>
2020-07-17 17:44:19 +0900

    hansol 사용자 마이페이지 찜한 전문가 리스트 내역 뽑아오기

--- FinalEeum/src/main/java/com/kh/eeum/service/LikeService.java
+++ FinalEeum/src/main/java/com/kh/eeum/service/LikeService.java
@@ -8,5 +8,7 @@ public interface LikeService {
 	int insertLike(Like like);
 	int selectLike(String expert_id, String user_id);
 	void deleteLike(Like like);
+	int wishlistCount(String user_id);
+	List<Object> wishlist(String user_id, int page, int limit);
 	
 }
